a0ddede9e458c8eedb5e9a4c47a3225b3c20c424
Yuya Satake <sata.yu.7878@gmail.com>
2023-04-21 18:44:25 -0700

    delete unnecessary file

--- src/routes/contactRoute 2.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { contactAdmin } from '../controllers/contactController';
-
-const router = express.Router();
-
-router.post('/', contactAdmin);
-
-export default router;
